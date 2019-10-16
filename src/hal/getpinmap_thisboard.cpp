c4b86c91c38f95b4fcee35f548e6717b28450999
JackGruber <24863925+JackGruber@users.noreply.github.com>
2019-10-16 18:27:45 +0200

    Add HalConfiguration TTGO_LoRa32_V1

--- src/hal/getpinmap_thisboard.cpp
+++ src/hal/getpinmap_thisboard.cpp
@@ -55,6 +55,8 @@ const HalPinmap_t *GetPinmap_ThisBoard(void)
         return GetPinmap_Catena4630();
 #elif defined(ARDUINO_MCCI_CATENA_4801)
         return GetPinmap_Catena4801();
+#elif defined(ARDUINO_TTGO_LoRa32_V1)
+        return GetPinmap_ttgo_lora32_v1();
 #elif defined(PINNOCHIO_SCOUT)
         return GetPinmap_PinnochioScount();
 #else
